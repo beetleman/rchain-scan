88637764b07b265e11829dfc428632204b97c010
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2018-10-28 11:13:21 +0100

    fix revord imports

--- src/clj/rchain_scan/specs.clj
+++ src/clj/rchain_scan/specs.clj
@@ -1,6 +1,8 @@
 (ns rchain-scan.specs
-  (:require [spec-tools.core :as st])
+  (:require [spec-tools.core :as st]
+            clojure.core.async.impl.channels)
   (:import [clojure.core.async.impl.channels ManyToManyChannel]))
+
 
 (defn channel-spec [i]
   (st/spec #(instance?  %)
